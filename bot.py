c4aed0078c2c2753e03e87efa3b8559d75ba56ae
Jelle Besseling <jelle@pingiun.com>
2019-02-10 18:31:36 +0100

    Add license preamble

--- bot.py
+++ bot.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python3
 
+# what-are-we-eating-today - a slack polling bot for food
+# Copyright (C) 2019 Jelle Besseling
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import os
 import random
 import sqlite3
